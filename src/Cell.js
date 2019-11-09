8fff097719cd222e5c18296df85dff96bc24ff53
DININTA ISNURTHINA <19058291@dinidininta.local>
2019-11-10 03:41:13 +0700

    attempted to generate neighbours in Cell class using for loop

--- src/Cell.js
+++ src/Cell.js
@@ -5,6 +5,19 @@ class Cell {
     this._initialStatus = initialStatus;
   }
 
+  generateNeighbours() {
+    const neighbours = [];
+    for (let i = this._x - 1; i < this._x + 2; i += 1) {
+      for (let j = this._y - 1; j < this._y + 2; j += 1) {
+        if (!(i === this._x && j === this._y)) {
+          neighbours.push(new Cell(i, j, Cell.DEAD));
+        }
+      }
+    }
+
+    return neighbours;
+  }
+
   status(neighbours) {
     const aliveNeighbours = neighbours.filter(neighbour => neighbour._initialStatus === Cell.ALIVE);
     if (this._initialStatus === Cell.DEAD || aliveNeighbours.length < 2) {
--- test/Cell.test.js
+++ test/Cell.test.js
@@ -13,4 +13,23 @@ describe('Cell', () => {
       expect(actualResult).toEqual(expectedResult);
     });
   });
+  describe('#generateNeighbours', () => {
+    it('should return corresponding 8 neighbours when input is one cell', () => {
+      const expectedResult = [
+        new Cell(0, 0, Cell.DEAD),
+        new Cell(0, 1, Cell.DEAD),
+        new Cell(0, 2, Cell.DEAD),
+        new Cell(1, 0, Cell.DEAD),
+        new Cell(1, 2, Cell.DEAD),
+        new Cell(2, 0, Cell.DEAD),
+        new Cell(2, 1, Cell.DEAD),
+        new Cell(2, 2, Cell.DEAD)
+      ];
+      const cell = new Cell(1, 1, Cell.ALIVE);
+
+      const actualResult = cell.generateNeighbours();
+
+      expect(actualResult).toEqual(expectedResult);
+    });
+  });
 });
