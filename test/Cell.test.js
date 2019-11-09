08e79607c01fdf801bca5daa4d8f27f5482c5b35
DININTA ISNURTHINA <19058291@dinidininta.local>
2019-11-09 20:25:40 +0700

    corrected first two test cases description in Cell test

--- test/Cell.test.js
+++ test/Cell.test.js
@@ -2,7 +2,7 @@ import Cell from '../src/Cell';
 
 describe('Cell', () => {
   describe('#status', () => {
-    it('should return Cell.ALIVE when condition of alive is met', () => {
+    it('should return Cell.ALIVE when initial status is Cell.ALIVE', () => {
       const expectedResult = Cell.ALIVE;
       const cell = new Cell(1, 1, Cell.ALIVE);
 
@@ -11,7 +11,7 @@ describe('Cell', () => {
       expect(actualResult).toEqual(expectedResult);
     });
 
-    it('should return Cell.DEAD when condition of dead cell is met', () => {
+    it('should return Cell.DEAD when initial status is Cell.DEAD', () => {
       const expectedResult = Cell.DEAD;
       const cell = new Cell(1, 1, Cell.DEAD);
 
