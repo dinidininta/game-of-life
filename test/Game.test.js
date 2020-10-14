31dc672f5a8d67e6d7d314da767d1321ffb873e7
Dininta Isnurthina <dini.dininta@gmail.com>
2020-10-14 15:38:12 +0700

    Create tests for Glider pattern after 4 ticks

--- test/Game.test.js
+++ test/Game.test.js
@@ -19,5 +19,80 @@ describe('#Game', () => {
 
       expect(actualResult).toEqual(expectedResult);
     });
+    it('should return cell coordinates that are still alive for Glider pattern after 2 ticks', () => {
+      const initialBoard = [
+        ['-', '-', '-', '-', '-', '-', '-'],
+        ['-', '-', '-', '-', '-', '-', '-'],
+        ['-', '-', '-', 'X', '-', '-', '-'],
+        ['-', '-', '-', '-', 'X', '-', '-'],
+        ['-', '-', 'X', 'X', 'X', '-', '-'],
+        ['-', '-', '-', '-', '-', '-', '-'],
+        ['-', '-', '-', '-', '-', '-', '-']
+      ];
+      const expectedResult = [
+        ['-', '-', '-', '-', '-', '-', '-'],
+        ['-', '-', '-', '-', '-', '-', '-'],
+        ['-', '-', '-', '-', '-', '-', '-'],
+        ['-', '-', '-', '-', 'X', '-', '-'],
+        ['-', '-', 'X', '-', 'X', '-', '-'],
+        ['-', '-', '-', 'X', 'X', '-', '-'],
+        ['-', '-', '-', '-', '-', '-', '-']
+      ];
+
+      const game = new Game(initialBoard);
+      const actualResult = game.simulateForNthTicks(2);
+
+      expect(actualResult).toEqual(expectedResult);
+    });
+    it('should return cell coordinates that are still alive for Glider pattern after 3 ticks', () => {
+      const initialBoard = [
+        ['-', '-', '-', '-', '-', '-', '-'],
+        ['-', '-', '-', '-', '-', '-', '-'],
+        ['-', '-', '-', 'X', '-', '-', '-'],
+        ['-', '-', '-', '-', 'X', '-', '-'],
+        ['-', '-', 'X', 'X', 'X', '-', '-'],
+        ['-', '-', '-', '-', '-', '-', '-'],
+        ['-', '-', '-', '-', '-', '-', '-']
+      ];
+      const expectedResult = [
+        ['-', '-', '-', '-', '-', '-', '-'],
+        ['-', '-', '-', '-', '-', '-', '-'],
+        ['-', '-', '-', '-', '-', '-', '-'],
+        ['-', '-', '-', 'X', '-', '-', '-'],
+        ['-', '-', '-', '-', 'X', 'X', '-'],
+        ['-', '-', '-', 'X', 'X', '-', '-'],
+        ['-', '-', '-', '-', '-', '-', '-']
+      ];
+
+      const game = new Game(initialBoard);
+      const actualResult = game.simulateForNthTicks(3);
+
+      expect(actualResult).toEqual(expectedResult);
+    });
+    it('should return cell coordinates that are still alive for Glider pattern after 4 ticks', () => {
+      const initialBoard = [
+        ['-', '-', '-', '-', '-', '-', '-'],
+        ['-', '-', '-', '-', '-', '-', '-'],
+        ['-', '-', '-', 'X', '-', '-', '-'],
+        ['-', '-', '-', '-', 'X', '-', '-'],
+        ['-', '-', 'X', 'X', 'X', '-', '-'],
+        ['-', '-', '-', '-', '-', '-', '-'],
+        ['-', '-', '-', '-', '-', '-', '-']
+      ];
+      const expectedResult = [
+        ['-', '-', '-', '-', '-', '-', '-'],
+        ['-', '-', '-', '-', '-', '-', '-'],
+        ['-', '-', '-', '-', '-', '-', '-'],
+        ['-', '-', '-', '-', 'X', '-', '-'],
+        ['-', '-', '-', '-', '-', 'X', '-'],
+        ['-', '-', '-', 'X', 'X', 'X', '-'],
+        ['-', '-', '-', '-', '-', '-', '-']
+      ];
+
+      const game = new Game(initialBoard);
+      const actualResult = game.simulateForNthTicks(4);
+
+      expect(actualResult).toEqual(expectedResult);
+    });
   });
 });
