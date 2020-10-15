5af3781bf3c8772b53d35b3167cf76864b3e43e0
Dininta Isnurthina <dini.dininta@gmail.com>
2020-10-15 14:31:09 +0700

    Add p5.js for visualisation

---
+++ src/Sketch.js
@@ -0,0 +1,10 @@
+// eslint-disable-next-line no-unused-vars
+function setup() {
+  createCanvas(400, 400);
+}
+
+// eslint-disable-next-line no-unused-vars
+function draw() {
+  background(220);
+  ellipse(50, 50, 80, 80);
+}
