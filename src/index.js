159fde4b8e1a74a41489697e71f7030a908420af
Dininta Isnurthina <dini.dininta@gmail.com>
2020-10-19 23:35:57 +0700

    Create render implementation using p5 in index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,46 @@
-import Game from './Game';
+/* eslint-disable no-param-reassign,new-cap,no-undef */
+import p5 from 'p5';
+import Board from './Board';
+import State from './State';
 
-const board = [
-  ['-', '-', '-', '-', '-', '-', '-', '-', '-', '-'],
-  ['-', '-', '-', '-', '-', '-', '-', '-', '-', '-'],
-  ['-', '-', '-', '-', '-', '-', '-', '-', '-', '-'],
-  ['-', '-', '-', '-', '-', '-', '-', '-', '-', '-'],
-  ['-', '-', '-', '-', '-', '-', '-', '-', '-', '-'],
-  ['-', '-', '-', '-', '-', '-', '-', '-', '-', '-'],
-  ['-', '-', '-', '-', '-', '-', '-', '-', '-', '-'],
-  ['-', '-', '-', '-', '-', '-', '-', '-', '-', '-'],
-  ['-', '-', '-', '-', '-', '-', '-', '-', 'X', 'X'],
-  ['-', '-', '-', '-', '-', '-', '-', '-', 'X', 'X']
+const resolution = 40;
+let board = [
+  ['-', '-', '-', '-', '-', '-', '-'],
+  ['-', '-', '-', '-', '-', '-', '-'],
+  ['-', '-', '-', '-', '-', '-', '-'],
+  ['-', '-', 'X', 'X', 'X', '-', '-'],
+  ['-', '-', '-', '-', '-', '-', '-'],
+  ['-', '-', '-', '-', '-', '-', '-'],
+  ['-', '-', '-', '-', '-', '-', '-']
 ];
-const game = new Game(board);
-game.simulateForNthTicks(1);
+
+const drawBoard = (processing) => {
+  board.forEach((row, i) => {
+    row.forEach((cell, j) => {
+      const x = i * resolution;
+      const y = j * resolution;
+      if (cell === State.ALIVE) {
+        processing.fill(255);
+        processing.rect(x, y, resolution - 1, resolution - 1);
+      }
+    });
+  });
+};
+
+const render = () => {
+  const sketch = (processing) => {
+    processing.setup = () => {
+      processing.createCanvas(280, 280);
+    };
+
+    processing.draw = () => {
+      processing.background(0);
+      drawBoard(processing);
+      board = new Board(board).getNextBoard();
+    };
+  };
+
+  return new p5(sketch);
+};
+
+render();
