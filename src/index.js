c80c11e0510adbfde7369154e3c612eedf33d06d
Dininta Isnurthina <dini.dininta@gmail.com>
2020-10-20 02:49:29 +0700

    Create initateBoard function to make fixed initial pattern

--- src/index.js
+++ src/index.js
@@ -25,8 +25,29 @@ const state = () => {
 const createBoard = (rows, columns) => Array.from({ length: rows }, () =>
   (Array.from({ length: columns }, () => state())));
 
+const initiateBoard = (rows, columns, aliveCells) => {
+  const init = Array.from({ length: rows }, () =>
+    (Array.from({ length: columns }, () => State.DEAD)));
+  aliveCells.forEach((cell) => {
+    const x = cell[0];
+    const y = cell[1];
+    init[x][y] = State.ALIVE;
+  });
+  return init;
+};
+
+const rows = window.innerWidth / resolution;
+const columns = window.innerHeight / resolution;
 // let board = createBoard(7, 7);
-let board = createBoard(window.innerWidth / resolution, window.innerHeight / resolution);
+// let board = createBoard(rows, columns);
+
+const aliveCells = [ // Glider Run
+  [1, 5], [1, 6], [2, 5], [2, 6], [11, 5], [11, 6], [11, 7], [12, 4], [12, 8],
+  [13, 3], [13, 9], [14, 3], [14, 9], [15, 6], [16, 4], [16, 8], [17, 5], [17, 6],
+  [17, 7], [18, 6], [21, 3], [21, 4], [21, 5], [22, 3], [22, 4], [22, 5], [23, 2],
+  [23, 6], [25, 1], [25, 2], [25, 6], [25, 7], [35, 3], [35, 4], [36, 3], [36, 4]
+];
+let board = initiateBoard(rows, columns, aliveCells);
 
 const drawBoard = (processing) => {
   board.forEach((row, i) => {
