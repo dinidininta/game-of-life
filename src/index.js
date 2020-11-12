9f79e2d3ea980b6a1a4e15b3935bacf4c2ceab43
Dininta Isnurthina <dini.dininta@gmail.com>
2020-11-13 00:06:51 +0700

    Add function drawEmptyBoard to draw initial white checkers. Change pattern generation and gameplay trigger.

--- src/index.js
+++ src/index.js
@@ -3,25 +3,31 @@ import p5 from 'p5';
 import Board from './Board';
 import State from './State';
 import Util from './Util';
-import Patterns from './Patterns';
+// import Patterns from './Patterns';
 
 const resolution = 20;
-// let board = [
-//   ['-', '-', '-', '-', '-', '-', '-'],
-//   ['-', '-', '-', '-', '-', '-', '-'],
-//   ['-', '-', '-', '-', '-', '-', '-'],
-//   ['-', '-', 'X', 'X', 'X', '-', '-'],
-//   ['-', '-', '-', '-', '-', '-', '-'],
-//   ['-', '-', '-', '-', '-', '-', '-'],
-//   ['-', '-', '-', '-', '-', '-', '-']
-// ];
-// let board = createBoard(7, 7);
-// let board = createBoard(rows, columns);
 
 const rows = window.innerWidth / resolution;
 const columns = window.innerHeight / resolution;
-let board = Util.initiateBoard(rows, columns, Patterns.PENTADECATHLON);
-// let board = Util.initiateBoard(rows, columns); // Random
+// let board = createBoard(7, 7);
+// let board = Util.initiateBoard(rows, columns, Patterns.GLIDER_RUN);
+// Random Pattern
+// let board = Util.initiateBoard(rows, columns);
+const pattern = [];
+let board;
+
+const drawEmptyBoard = (processing) => {
+  for (let i = 0; i < rows; i += 1) {
+    for (let j = 0; j < columns; j += 1) {
+      const x = i * resolution;
+      const y = j * resolution;
+      processing.fill(255);
+      processing.stroke(0);
+      processing.strokeWeight(1);
+      processing.rect(x, y, resolution - 1, resolution - 1);
+    }
+  }
+};
 
 const drawPattern = (processing) => {
   board.forEach((row, i) => {
@@ -36,33 +42,28 @@ const drawPattern = (processing) => {
   });
 };
 
-const drawBoard = (processing) => {
-  for (let i = 0; i < rows; i += 1) {
-    for (let j = 0; j < columns; j += 1) {
-      const x = i * resolution;
-      const y = j * resolution;
-      processing.fill(255);
-      processing.stroke(0);
-      processing.strokeWeight(1);
-      processing.rect(x, y, resolution - 1, resolution - 1);
-    }
-  }
-};
-
 const render = () => {
   const sketch = (processing) => {
     processing.setup = () => {
       // processing.createCanvas(280, 280);
       processing.createCanvas(window.innerWidth, window.innerHeight);
       processing.frameRate(10);
+      processing.background(0);
+      drawEmptyBoard(processing);
     };
 
     processing.draw = () => {
-      processing.background(0);
-      drawBoard(processing);
-      if (processing.keyCode === processing.ENTER) {
+      if (processing.mouseIsPressed) {
+        const x = Util.estimatePoint(processing.mouseX) / resolution;
+        const y = Util.estimatePoint(processing.mouseY) / resolution;
+        pattern.push([x, y]);
+        board = Util.initiateBoard(rows, columns, pattern);
         drawPattern(processing);
+      }
+      if (processing.keyCode === processing.ENTER) {
+        drawEmptyBoard(processing);
         board = new Board(board).getNextBoard();
+        drawPattern(processing);
       }
     };
   };
